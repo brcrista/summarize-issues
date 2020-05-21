3c11185b248c4eb29deba75571431b53ca51b053
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-05-21 15:59:21 -0400

    use array equals

--- test/markdown.test.ts
+++ test/markdown.test.ts
@@ -3,10 +3,8 @@ import type { Section } from '../src/types';
 
 test('Generate empty details', () => {
     const details = Array.from(markdown.generateDetails([], { owner: 'test', repo: 'repo' }));
-    console.log(details);
 
-    expect(details.length).toEqual(1);
-    expect(details[0]).toEqual('## Details');
+    expect(details).toStrictEqual(['## Details']);
 });
 
 test('Generate details for a section with no issues', () => {
@@ -19,14 +17,14 @@ test('Generate details for a section with no issues', () => {
     }];
 
     const details = Array.from(markdown.generateDetails(sections, { owner: 'test', repo: 'repo' }));
-    console.log(details);
 
-    expect(details.length).toEqual(7);
-    expect(details[0]).toEqual('## Details');
-    expect(details[1]).toEqual('### 💚🥳 Empty section [(query)](https://github.com/test/repo/issues?q=is%3Aissue+is%3Aopen)');
-    expect(details[2]).toEqual('Total: 0\n');
-    expect(details[3]).toEqual('Threshold: 1\n');
-    expect(details[4]).toEqual('Labels: \n');
-    expect(details[5]).toEqual('| Owner | Count |');
-    expect(details[6]).toEqual('| -- | -- |');
+    expect(details).toStrictEqual([
+        '## Details',
+        '### 💚🥳 Empty section [(query)](https://github.com/test/repo/issues?q=is%3Aissue+is%3Aopen)',
+        'Total: 0\n',
+        'Threshold: 1\n',
+        'Labels: \n',
+        '| Owner | Count |',
+        '| -- | -- |'
+    ]);
 });
