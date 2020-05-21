e7b7b6892817602ffff982395d854dadf6e00e0d
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-05-21 16:02:36 -0400

    Use describe()

--- test/markdown.test.ts
+++ test/markdown.test.ts
@@ -1,30 +1,32 @@
 import * as markdown from '../src/markdown';
 import type { Section } from '../src/types';
 
-test('Generate empty details', () => {
-    const details = Array.from(markdown.generateDetails([], { owner: 'test', repo: 'repo' }));
+describe('The details section', () => {
+    test('is empty when there are no sections', () => {
+        const details = Array.from(markdown.generateDetails([], { owner: 'test', repo: 'repo' }));
 
-    expect(details).toStrictEqual(['## Details']);
-});
+        expect(details).toStrictEqual(['## Details']);
+    });
 
-test('Generate details for a section with no issues', () => {
-    const sections: Section[] = [{
-        section: "Empty section",
-        labels: [],
-        threshold: 1,
-        issues: [],
-        status: '💚🥳'
-    }];
+    test('has an empty table when there are no issues', () => {
+        const sections: Section[] = [{
+            section: "Empty section",
+            labels: [],
+            threshold: 1,
+            issues: [],
+            status: '💚🥳'
+        }];
 
-    const details = Array.from(markdown.generateDetails(sections, { owner: 'test', repo: 'repo' }));
+        const details = Array.from(markdown.generateDetails(sections, { owner: 'test', repo: 'repo' }));
 
-    expect(details).toStrictEqual([
-        '## Details',
-        '### 💚🥳 Empty section [(query)](https://github.com/test/repo/issues?q=is%3Aissue+is%3Aopen)',
-        'Total: 0\n',
-        'Threshold: 1\n',
-        'Labels: \n',
-        '| Owner | Count |',
-        '| -- | -- |'
-    ]);
+        expect(details).toStrictEqual([
+            '## Details',
+            '### 💚🥳 Empty section [(query)](https://github.com/test/repo/issues?q=is%3Aissue+is%3Aopen)',
+            'Total: 0\n',
+            'Threshold: 1\n',
+            'Labels: \n',
+            '| Owner | Count |',
+            '| -- | -- |'
+        ]);
+    });
 });
